472aa3df702bfe0eb41bf5edadc74cb2cb5a824c
sbsteacher <id@gmail.com>
2026-01-27 12:20:51 +0900

    insert 성공(2)

--- src/main/java/com/green/board/application/BoardController.java
+++ src/main/java/com/green/board/application/BoardController.java
@@ -5,10 +5,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
 
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    /* 파라미터(매개변수)는 FE부터 날라오는 데이터를 받는 용도로 사용
+           @RequestBody는 JSON형태로 BODY에 담겨져있는 데이터를 받을 때 사용.
+           JSON형태와 일치하는 클래스를 만들어 주면 된다. (필드명과 Key값이 같아야 한다)
+         */
     @PostMapping("/board")
     public String postBoard(@RequestBody BoardPostReq req) {
         System.out.println("req: " + req);
--- src/main/java/com/green/board/application/BoardMapper.java
+++ src/main/java/com/green/board/application/BoardMapper.java
@@ -8,6 +8,9 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper 애노테이션은 두 파일(인터페이스, xml)의 내용으로 인터페이스를 implements한 클래스를
 만들고 그 클래스를 빈 등록한다. 빈 등록을 하면 스프링 컨테이너가 해당 클래스를 싱글톤으로
 객체화하고 주소값을 가지고 있는다. 그리고 주소값을 달라고하면 DI해준다.
+
+insert, update, delete 즉, select를 제외하고는 리턴타입 int하시면 된다.
+int값이 affectedRows값이다. 영향받은 행 값
 */
 @Mapper
 public interface BoardMapper {
