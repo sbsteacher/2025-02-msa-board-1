12395b9ebbc70f2858d271b61aa6501a546966d1
sbsteacher <id@gmail.com>
2026-01-29 12:38:04 +0900

    save return 정수

--- src/main/java/com/green/board/application/BoardController.java
+++ src/main/java/com/green/board/application/BoardController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("board")
 public class BoardController {
     private final BoardService boardService;
 
@@ -14,31 +15,31 @@ public class BoardController {
            @RequestBody는 JSON형태로 BODY에 담겨져있는 데이터를 받을 때 사용.
            JSON형태와 일치하는 클래스를 만들어 주면 된다. (필드명과 Key값이 같아야 한다)
          */
-    @PostMapping("/board")
-    public String postBoard(@RequestBody BoardPostReq req) {
+    @PostMapping
+    public int postBoard(@RequestBody BoardPostReq req) {
         System.out.println("req: " + req);
         int result = boardService.postBoard(req);
-        return result == 1 ? "성공" : "실패";
+        return result;
     }
 
-    @GetMapping("/board")
+    @GetMapping
     public List<BoardGetRes> getBoardList() {
         return boardService.getBoardList();
     }
 
-    @GetMapping("/board/{id}")
+    @GetMapping("{id}")
     public BoardGetOneRes getBoardOne(@PathVariable int id) {
         System.out.println("id: " + id);
         return boardService.getBoardOne(id);
     }
 
-    @PutMapping("/board")
+    @PutMapping
     public int putBoard(@RequestBody BoardPutReq req) {
         System.out.println("req: " + req);
         return boardService.putBoard(req);
     }
 
-    @DeleteMapping("/board")
+    @DeleteMapping
     public int delBoard(@RequestParam int id) {
         System.out.println("delete-id: " + id);
         return boardService.delBoard(id);
